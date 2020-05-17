594e0b709c72d67136aee041153899daef51e37a
Infarh <shmachilin@yandex.ru>
2020-05-17 15:08:09 +0300

    Тестируем использование элементов WPF в библиотеках

---
+++ Tests/TestLibFramework/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestLibFramework
+{
+    public abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+}

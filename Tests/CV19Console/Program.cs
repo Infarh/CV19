a0a9ab8445ddc9e1f70abcba6d4cb277eaf239eb
Infarh <shmachilin@yandex.ru>
2020-05-17 16:32:36 +0300

    Извлечение данных по датам записей

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
-using System.Net;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
@@ -8,19 +12,56 @@ namespace CV19Console
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if(string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+           .First()
+           .Split(',')
+           .Skip(4)
+           .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+           .ToArray();
+
+
+
         static void Main(string[] args)
         {
             //var web_client = new WebClient();
 
-            var client = new HttpClient();
+            //var client = new HttpClient();
 
             //var items = new string[10];
 
             //var last_item = items[^1];
             //var prev_last_item = items[^2];
 
-            var response = client.GetAsync(data_url).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
+            //var response = client.GetAsync(data_url).Result;
+            //var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            //foreach (var data_line in GetDataLines())
+            //    Console.WriteLine(data_line);
+
+            var dates = GetDates();
+
+            Console.WriteLine(string.Join("\r\n", dates));
+
 
             Console.ReadLine();
         }

667ec19b37ca6a5f16237c62696094ea5aa9734e
Infarh <shmachilin@yandex.ru>
2020-05-17 15:02:18 +0300

    Базовый класс команды

---
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+        public abstract void Execute(object parameter);
+    }
+}

1d280ec2cc2027770f015c84f1b2971c8babc81b
Infarh <shmachilin@yandex.ru>
2020-05-17 15:16:02 +0300

    Клавный класс команды для нашего приложения

---
+++ CV19/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CV19.Infrastructure.Commands.Base;
+
+namespace CV19.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}

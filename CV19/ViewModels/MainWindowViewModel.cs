c2a5f0f5a9f08f805cca38a7e16d9c5365a9629d
Infarh <shmachilin@yandex.ru>
2020-05-20 19:56:25 +0300

    Добавляем понятие "Выбранная группа" в модель-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -18,6 +18,20 @@ namespace CV19.ViewModels
 
         public ObservableCollection<Group> Groups { get; }
 
+        #region SelectedGroup : Group - Выбранная группа
+
+        /// <summary>Выбранная группа</summary>
+        private Group _SelectedGroup;
+
+        /// <summary>Выбранная группа</summary>
+        public Group SelectedGroup
+        {
+            get => _SelectedGroup;
+            set => Set(ref _SelectedGroup, value);
+        }
+
+        #endregion
+
         #region SelectedPageIndex : int - Номер выбранной вкладки
 
         /// <summary>Номер выбранной вкладки</summary>

c20074ba61c2260a2940d4de8e2783b3b0bf3abc
Infarh <shmachilin@yandex.ru>
2020-05-20 19:37:19 +0300

    Тестовая предметная область

---
+++ CV19/Models/Decanat/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CV19.Models.Decanat
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public double Rating { get; set; }
+
+
+    }
+
+    internal class Group
+    {
+        public string Name { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}

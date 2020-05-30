f73eee98ae38dd373d4f233467d2a89f8b7bc602
Infarh <shmachilin@yandex.ru>
2020-05-30 15:18:51 +0300

    Больше конвертеров хороших и разных! Композитный конвертер

---
+++ CV19/Infrastructure/Converters/CompositeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class CompositeConverter : Converter
+    {
+        public IValueConverter First { get; set; }
+
+        public IValueConverter Second { get; set; }
+
+
+        public override object Convert(object v, Type t, object p, CultureInfo c)
+        {
+            var result1 = First?.Convert(v, t, p, c) ?? v;
+            var result2 = Second?.Convert(result1, t, p, c) ?? result1;
+
+            return result2;
+
+        }
+
+        public override object ConvertBack(object v, Type t, object p, CultureInfo c)
+        {
+            var result2 = Second?.ConvertBack(v, t, p, c) ?? v;
+            var result1 = First?.ConvertBack(result2, t, p, c) ?? result2;
+
+            return result1;
+        }
+    }
+}
--- CV19/Infrastructure/Converters/LocationPointToStr.cs
+++ CV19/Infrastructure/Converters/LocationPointToStr.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 
 namespace CV19.Infrastructure.Converters
 {
+    [ValueConversion(typeof(Point), typeof(string))]
     internal class LocationPointToStr : Converter
     {
         public override object Convert(object value, Type t, object p, CultureInfo c)

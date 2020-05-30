c581deed0365b0433df8e225df361bd9fbc7f647
Infarh <shmachilin@yandex.ru>
2020-05-30 15:30:28 +0300

    Мульти-конвертер

---
+++ CV19/Infrastructure/Converters/MultiConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] vv, Type t, object p, CultureInfo c);
+
+        public virtual object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) => 
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+    }
+}
---
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal class ToArray : MultiConverter
+    {
+        public override object Convert(object[] vv, Type t, object p, CultureInfo c)
+        {
+            var collection = new CompositeCollection();
+            foreach (var value in vv)
+                collection.Add(value);
+            return collection;
+        }
+
+        //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) => v as object[];
+    }
+}

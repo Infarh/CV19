a40d54943bbc170e697dca327b28b5b13ee92958
Infarh <shmachilin@yandex.ru>
2020-06-06 15:10:06 +0300

    Запуск и остановка хоста при старте и выходе из приложения

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Media;
 using CV19.Services;
@@ -11,11 +12,28 @@ namespace CV19
     public partial class App : Application
     {
         public static bool IsDesignMode { get; private set; } = true;
-        
-        protected override void OnStartup(StartupEventArgs e)
+
+        private static IHost __Host;
+
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
             IsDesignMode = false;
+            var host = Host;
             base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            __Host = null;
         }
 
         public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)

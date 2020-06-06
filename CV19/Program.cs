a8183e235ea31eb5e5426fc9f77750df9700e204
Infarh <shmachilin@yandex.ru>
2020-06-06 14:57:11 +0300

    Собственная точка входа в приложение - метод Program.Main

---
+++ CV19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CV19
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}

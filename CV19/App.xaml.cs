e615fbe69fc41c951f9445cd866f0a2ca735e3c9
Infarh <shmachilin@yandex.ru>
2020-06-06 15:06:25 +0300

    Инфраструктура для создания хоста приложения

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -2,17 +2,26 @@
 using System.Windows;
 using System.Windows.Media;
 using CV19.Services;
+using CV19.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CV19
 {
     public partial class App : Application
     {
         public static bool IsDesignMode { get; private set; } = true;
-
+        
         protected override void OnStartup(StartupEventArgs e)
         {
             IsDesignMode = false;
             base.OnStartup(e);
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<DataService>();
+            services.AddSingleton<CountriesStatisticViewModel>();
         }
     }
 }
--- CV19/Program.cs
+++ CV19/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace CV19
 {
@@ -10,6 +12,23 @@ namespace CV19
             var app = new App();
             app.InitializeComponent();
             app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var host_builder = Host.CreateDefaultBuilder(Args);
+
+            host_builder.UseContentRoot(Environment.CurrentDirectory);
+            host_builder.ConfigureAppConfiguration((host, cfg) =>
+            {
+                cfg.SetBasePath(Environment.CurrentDirectory);
+                cfg.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            });
+
+            host_builder.ConfigureServices(App.ConfigureServices);
+
+
+            return host_builder;
         }
     }
 }

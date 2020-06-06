0f17f4fb97688df27e21676652034d7628de8114
Infarh <shmachilin@yandex.ru>
2020-06-06 15:39:31 +0300

    Хлам!

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace CV19
 {
-    public partial class App : Application
+    public partial class App
     {
         public static bool IsDesignMode { get; private set; } = true;
 

8654babef1bc188108ed746b0aeae7b11ddb69b3
Infarh <shmachilin@yandex.ru>
2020-06-20 21:21:19 +0300

    Представление и модель-представления для Web-сервера

---
+++ CV19/ViewModels/WebServerViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Input;
+using CV19.Infrastructure.Commands;
+using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class WebServerViewModel : ViewModel
+    {
+        #region Enabled
+
+        private bool _Enabled;
+
+        public bool Enabled { get => _Enabled; set => Set(ref _Enabled, value); }
+
+        #endregion
+
+        #region StartCommand
+
+        private ICommand _StartCommand;
+
+        public ICommand StartCommand => _StartCommand
+            ??= new LambdaCommand(OnStartCommandExecuted, CanStartCommandExecute);
+
+        private bool CanStartCommandExecute(object p) => !_Enabled;
+
+        private void OnStartCommandExecuted(object p)
+        {
+            Enabled = true;
+        }
+
+        #endregion
+
+        #region StopCommand
+
+        private ICommand _StopCommand;
+
+        public ICommand StopCommand => _StopCommand
+            ??= new LambdaCommand(OnStopCommandExecuted, CanStopCommandExecute);
+
+        private bool CanStopCommandExecute(object p) => _Enabled;
+
+        private void OnStopCommandExecuted(object p)
+        {
+            Enabled = false;
+        }
+
+        #endregion
+    }
+}

863052ac66873f78d82fe65ccb43c13ebed4a95e
Infarh <shmachilin@yandex.ru>
2020-06-20 19:56:10 +0300

    Блокировка критической секции

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 
@@ -10,26 +12,60 @@ namespace CV19Console
         {
             Thread.CurrentThread.Name = "Main theread";
 
-            var thread = new Thread(ThreadMethod);
-            thread.Name = "Other thread";
-            thread.IsBackground = true;
-            thread.Priority = ThreadPriority.AboveNormal;
+            //var thread = new Thread(ThreadMethod);
+            //thread.Name = "Other thread";
+            //thread.IsBackground = true;
+            //thread.Priority = ThreadPriority.AboveNormal;
 
-            thread.Start(42);
+            //thread.Start(42);
 
-            var count = 5;
-            var msg = "Hello World!";
-            var timeout = 150;
+            //var count = 5;
+            //var msg = "Hello World!";
+            //var timeout = 150;
 
-            new Thread(() => PrintMethod(msg, count, timeout)) { IsBackground = true }.Start();
+            //new Thread(() => PrintMethod(msg, count, timeout)) { IsBackground = true }.Start();
 
-            CheckThread();
 
-            for (var i = 0; i < 5; i++)
+
+            //CheckThread();
+
+            //for (var i = 0; i < 5; i++)
+            //{
+            //    Thread.Sleep(100);
+            //    Console.WriteLine(i);
+            //}
+
+            var values = new List<int>();
+
+            var threads = new Thread[10];
+            object lock_object = new object();
+            for (var i = 0; i < threads.Length; i++)
+                threads[i] = new Thread(() =>
+                {
+                    for (var j = 0; j < 10; j++)
+                    {
+                        lock (lock_object)
+                            values.Add(Thread.CurrentThread.ManagedThreadId);
+                        Thread.Sleep(1);
+                    }
+                });
+
+            Monitor.Enter(lock_object);
+            try
             {
-                Thread.Sleep(100);
-                Console.WriteLine(i);
+
             }
+            finally
+            {
+                Monitor.Exit(lock_object);
+            }
+
+            foreach (var thread in threads)
+                thread.Start();
+
+
+            Console.ReadLine();
+            Console.WriteLine(string.Join(",", values));
 
             Console.ReadLine();
         }

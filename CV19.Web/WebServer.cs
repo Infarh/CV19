2eb90ad2df59e676a323498d4c4839d3f3944ae4
Infarh <shmachilin@yandex.ru>
2020-06-20 20:47:27 +0300

    Заготовка асинхронного сервера

--- CV19.Web/WebServer.cs
+++ CV19.Web/WebServer.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Net;
 
 namespace CV19.Web
 {
     public class WebServer
     {
+        //private TcpListener _Listener = new TcpListener(new IPEndPoint(IPAddress.Any, 8080));
+        private HttpListener _Listener;
+        private readonly int _Port;
+        private bool _Enabled;
+        private readonly object _SyncRoot = new object();
+
+        public int Port => _Port;
+
+        public bool Enabled { get => _Enabled; set { if (value) Start(); else Stop(); } }
+
+        public WebServer(int Port) => _Port = Port;
+
+        public void Start()
+        {
+            if (_Enabled) return;
+            lock (_SyncRoot)
+            {
+                if (_Enabled) return;
+
+                _Listener = new HttpListener();
+                _Listener.Prefixes.Add($"http://*:{_Port}");
+                _Listener.Prefixes.Add($"http://+:{_Port}");
+                _Enabled = true;
+            }
+            Listen();
+        }
+
+        public void Stop()
+        {
+            if (!_Enabled) return;
+            lock (_SyncRoot)
+            {
+                if (!_Enabled) return;
+
+                _Listener = null;
+                _Enabled = false;
+            }
+        }
+
+        private void Listen()
+        {
+
+        }
     }
 }

32802fcb005929cb05f584c89cefaa7618ae6c75
Infarh <shmachilin@yandex.ru>
2020-06-27 16:11:48 +0300

    Обеспечиваем возможность перетаскивания окна за заголовок

---
+++ CV19/Infrastructure/Behaviors/WindowTitleBarBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19.Infrastructure.Behaviors
+{
+    class WindowTitleBarBehavior : Behavior<UIElement>
+    {
+        private Window _Window;
+
+        protected override void OnAttached()
+        {
+            _Window = AssociatedObject as Window ?? AssociatedObject.FindLogicalParent<Window>();
+            AssociatedObject.MouseLeftButtonDown += OnMouseDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseLeftButtonDown -= OnMouseDown;
+            _Window = null;
+        }
+
+        private void OnMouseDown(object Sender, MouseButtonEventArgs E)
+        {
+            if(E.ClickCount > 1) return;
+            if (!(AssociatedObject.FindVisualRoot() is Window window)) return;
+            window.DragMove();
+        }
+    }
+}

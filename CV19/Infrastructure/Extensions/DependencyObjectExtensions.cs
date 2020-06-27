60fad659fd32bc676fb8cfac67643e82e9a7d9a4
Infarh <shmachilin@yandex.ru>
2020-06-27 15:53:33 +0300

    Методы-расширения для DependencyObject, позволяющие найти его корень в дереве (логическом, или визуальном)

---
+++ CV19/Infrastructure/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+
+namespace System.Windows
+{
+    static class DependencyObjectExtensions
+    {
+        public static DependencyObject FindVisualRoot(this DependencyObject obj)
+        {
+            do
+            {
+                var parent = VisualTreeHelper.GetParent(obj);
+                if (parent is null) return obj;
+                obj = parent;
+            }
+            while (true);
+        }
+
+        public static DependencyObject FindLogicalRoot(this DependencyObject obj)
+        {
+            do
+            {
+                var parent = LogicalTreeHelper.GetParent(obj);
+                if (parent is null) return obj;
+                obj = parent;
+            }
+            while (true);
+        }
+    }
+}
